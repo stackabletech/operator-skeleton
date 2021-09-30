078899b5c2de3f6f6e1de5dd3a1f625133b21858
Sönke Liebau <soenke.liebau@stackable.de>
2021-09-30 13:17:23 +0200

    Added github actions and examples files

--- rename.sh
+++ rename.sh
@@ -14,8 +14,17 @@ find . -name "*.rs" -exec sed -i "s/productname/${LOWERNAME}/g" '{}' \;
 find . -name "*.rs" -exec sed -i "s/ProductName/${PRETTYNAME}/g" '{}' \;
 find . -name "*.rs" -exec sed -i "s/Productname/${VARNAME}/g" '{}' \;
 
+find . -name "*.yaml" -exec sed -i "s/productname/${LOWERNAME}/g" '{}' \;
+find . -name "*.yaml" -exec sed -i "s/ProductName/${PRETTYNAME}/g" '{}' \;
+find . -name "*.yaml" -exec sed -i "s/Productname/${VARNAME}/g" '{}' \;
+
+find . -name "*.yml" -exec sed -i "s/productname/${LOWERNAME}/g" '{}' \;
+find . -name "*.yml" -exec sed -i "s/ProductName/${PRETTYNAME}/g" '{}' \;
+find . -name "*.yml" -exec sed -i "s/Productname/${VARNAME}/g" '{}' \;
+
 sed -i "s/productname/${LOWERNAME}/g" 'docker/Dockerfile'
 sed -i "s/ProductName/${PRETTYNAME}/g" 'docker/Dockerfile'
 sed -i "s/Productname/${VARNAME}/g" 'docker/Dockerfile'
 
 mv rust/operator-binary/src/stackable-productname-operator.rs rust/operator-binary/src/stackable-${LOWERNAME}-operator.rs
+mv examples/simple-productname-cluster.yaml examples/simple-${LOWERNAME}-cluster.yaml
